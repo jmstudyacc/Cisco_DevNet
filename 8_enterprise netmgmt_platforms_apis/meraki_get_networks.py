3a587878f5d22cd0f967a8ca8a95b6bed66a638f
james miles <jmstudyacc@gmail.com>
2021-06-06 20:00:59 +0100

    meraki api calls

---
+++ 8_enterprise netmgmt_platforms_apis/meraki_get_networks.py
@@ -0,0 +1,25 @@
+import requests
+import json
+
+MERAKI_ORG = '681155'
+MERAKI_URL = f'https://api.meraki.com/api/v1/organizations/{MERAKI_ORG}/networks'
+MERAKI_X_AUTH = '6bec40cf957de430a6f1f2baa056b99a4fac9ea0'
+
+# payload is an empty dictionary
+payload = {}
+
+# define the necessary headers for Meraki
+headers = {'X-Cisco-Meraki-API-Key': MERAKI_X_AUTH,
+           'Accept': 'application/json',
+           'Content-type': 'application/json'
+           }
+
+# create a variable to store the response of upcoming request
+response = requests.request('GET', url=MERAKI_URL, headers=headers, data=payload)
+
+# print the status code and the response itself
+print(response.status_code)
+
+# print out the resulting networks
+print(json.dumps(response.json(), indent=2))
+
